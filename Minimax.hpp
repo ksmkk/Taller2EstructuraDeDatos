0083430e620a79a894680f46c1cbe25b0e159fd7
ksmkk <133812239+ksmkk@users.noreply.github.com>
2024-12-09 01:21:55 -0300

    mejorando detallitos

--- Minimax.hpp
+++ Minimax.hpp
@@ -1,9 +1,5 @@
 #ifndef MINIMAX_HPP
 #define MINIMAX_HPP
 
-int evaluar(char tablero[3][3], char simbolo);
 int minimax(char tablero[3][3], bool profundida, char simboloIA, char simboloJugador);
-void mejorMovimiento(char tablero[3][3], int& fila, int& columna, char simbolo);
-bool esEmpate(char tablero[3][3]);
-
 #endif
