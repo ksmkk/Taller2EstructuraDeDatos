104cdbb8e8e36717f1b9206912cd8397f453f82b
ksmkk <133812239+ksmkk@users.noreply.github.com>
2024-12-09 01:28:55 -0300

    AAAAAAAA

--- Minimax.cpp
+++ Minimax.cpp
@@ -1,38 +1,92 @@
 #include "Minimax.hpp"
-#include "Tablero.hpp"
+#include <limits>
 #include <algorithm>
 
-int minimax(char tablero[3][3], bool esMaximizador, char simboloIA, char simboloJugador) {
-    Tablero tempTablero;
-    int puntaje = tempTablero.evaluar(simboloIA);
-
-    if (puntaje == 10 || puntaje == -10 || tempTablero.esEmpate()) {
-        return puntaje;
+int evaluarTablero(char tablero[3][3], char simbolo) {
+    char oponente = (simbolo == 'X') ? 'O' : 'X';
+    for (int i = 0; i < 3; ++i) {
+        if (tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) {
+            if (tablero[i][0] == simbolo) return 10;
+            if (tablero[i][0] == oponente) return -10;
+        }
+        if (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) {
+            if (tablero[0][i] == simbolo) return 10;
+            if (tablero[0][i] == oponente) return -10;
+        }
     }
+    if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
+        if (tablero[0][0] == simbolo) return 10;
+        if (tablero[0][0] == oponente) return -10;
+    }
+    if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]) {
+        if (tablero[0][2] == simbolo) return 10;
+        if (tablero[0][2] == oponente) return -10;
+    }
+    return 0;
+}
 
+bool hayMovimientosDisponibles(char tablero[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (tablero[i][j] == ' ') return true;
+        }
+    }
+    return false;
+}
+
+int minimax(char tablero[3][3], int profundidad, bool esMaximizador, int alpha, int beta, char simbolo) {
+    int puntuacion = evaluarTablero(tablero, simbolo);
+    char oponente = (simbolo == 'X') ? 'O' : 'X';
+    if (puntuacion == 10) return puntuacion - profundidad;
+    if (puntuacion == -10) return puntuacion + profundidad;
+    if (!hayMovimientosDisponibles(tablero)) return 0;
     if (esMaximizador) {
-        int mejorPuntaje = -1000;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        int mejorValor = std::numeric_limits<int>::min();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
                 if (tablero[i][j] == ' ') {
-                    tablero[i][j] = simboloIA;
-                    mejorPuntaje = std::max(mejorPuntaje, minimax(tablero, false, simboloIA, simboloJugador));
+                    tablero[i][j] = simbolo;
+                    mejorValor = std::max(mejorValor, minimax(tablero, profundidad + 1, false, alpha, beta, simbolo));
                     tablero[i][j] = ' ';
+                    alpha = std::max(alpha, mejorValor);
+                    if (beta <= alpha) return mejorValor;
                 }
             }
         }
-        return mejorPuntaje;
+        return mejorValor;
     } else {
-        int mejorPuntaje = 1000;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        int mejorValor = std::numeric_limits<int>::max();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
                 if (tablero[i][j] == ' ') {
-                    tablero[i][j] = simboloJugador;
-                    mejorPuntaje = std::min(mejorPuntaje, minimax(tablero, true, simboloIA, simboloJugador));
+                    tablero[i][j] = oponente;
+                    mejorValor = std::min(mejorValor, minimax(tablero, profundidad + 1, true, alpha, beta, simbolo));
                     tablero[i][j] = ' ';
+                    beta = std::min(beta, mejorValor);
+                    if (beta <= alpha) return mejorValor;
                 }
             }
         }
-        return mejorPuntaje;
+        return mejorValor;
+    }
+}
+
+void mejorMovimiento(char tablero[3][3], int& fila, int& columna, char simbolo) {
+    int mejorValor = std::numeric_limits<int>::min();
+    fila = -1;
+    columna = -1;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (tablero[i][j] == ' ') {
+                tablero[i][j] = simbolo;
+                int valorMovimiento = minimax(tablero, 0, false, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), simbolo);
+                tablero[i][j] = ' ';
+                if (valorMovimiento > mejorValor) {
+                    mejorValor = valorMovimiento;
+                    fila = i;
+                    columna = j;
+                }
+            }
+        }
     }
 }
--- Minimax.hpp
+++ Minimax.hpp
@@ -1,5 +1,7 @@
 #ifndef MINIMAX_HPP
 #define MINIMAX_HPP
 
-int minimax(char tablero[3][3], bool profundida, char simboloIA, char simboloJugador);
+void mejorMovimiento(char tablero[3][3], int& fila, int& columna, char simbolo);
+int minimax(char tablero[3][3], int profundidad, bool esMaximizador, int alpha, int beta, char simbolo);
+
 #endif
